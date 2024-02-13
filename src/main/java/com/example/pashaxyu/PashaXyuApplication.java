33b05a9bbe47d933cc33149dbde96f1f9aa68637
PavelBoyko07 <pbojko67@gmail.com>
2024-02-13 17:51:55 +0300

    initial

---
+++ src/main/java/com/example/pashaxyu/PashaXyuApplication.java
@@ -0,0 +1,13 @@
+package com.example.pashaxyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PashaXyuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PashaXyuApplication.class, args);
+    }
+
+}
